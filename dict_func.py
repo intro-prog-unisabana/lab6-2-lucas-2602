24bffc93cbff386ebde910bfdc4b49b6a96cb4e3
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-27 07:21:25 -0500

    dict_func.py

--- dict_func.py
+++ dict_func.py
@@ -1,5 +1,23 @@
 # Write your code here!
 def employee_print(employee_info):
-    employee_info.get("Name", "N/A")
-    employee_info.get("Salary", "N/A")
-    employee_info.get("Role", "N/A")
+    name = employee_info.get("Name", "N/A")
+    salary = employee_info.get("Salary", "N/A")
+    role = employee_info.get("Role", "N/A")
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+    employee_info.pop("Name")
+    employee_info.pop("Salary")
+    employee_info.pop("Role")
+    print(employee_info)
+    print("No other info!")
+    
+
+employee_info = {
+    "Name": "Diego",
+    "Salary": 5000000,
+    "Role": "Janitor",
+    "Years at company": 9001,
+    "Hours per week": 2
+}
+employee_print(employee_info)
