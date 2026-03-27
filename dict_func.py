5f4365d5b486f803e80f2bb2a0433de07bfe7d05
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-27 06:37:30 -0500

    dict_func.py

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,5 @@
 # Write your code here!
+def employee_print(employee_info):
+    employee_info.get("Name", "N/A")
+    employee_info.get("Salary", "N/A")
+    employee_info.get("Role", "N/A")
--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,5 @@
 # Write your code here!
+def temp_and_color(datos):
+    valor1 = datos.get("temp")
+    valor2 = datos.get("color")
+    return (valor1, valor2)
