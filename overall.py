1fcb3a3713c329d4a79f9f54e30da1eec5b6ae81
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-28 22:13:07 -0500

    overall.py

--- overall.py
+++ overall.py
@@ -1,25 +1,31 @@
 def student_averages(students):
     promedios = {}
-    for estudiante, tareas in students.items():
-        notas = tareas.values()
-        promedio = sum(notas)/len(notas)
-        promedios[estudiante] = round(promedio)
-    return promedios
+    if not students:
+        return {}
+    else:
+        for estudiante, tareas in students.items():
+            notas = tareas.values()
+            promedio = sum(notas)/len(notas)
+            promedios[estudiante] = round(promedio)
+        return promedios
 
 def assignment_averages(students):
     tarea1 = []
     tarea2 = []
     tarea3 = []
-    for valor in students.values():
-        tarea1.append(valor["hw1"])
-        tarea2.append(valor["hw2"])
-        tarea3.append(valor["hw3"])
-    promedio1 = round(sum(tarea1)/len(tarea1))
-    promedio2 = round(sum(tarea2)/len(tarea2))
-    promedio3 = round(sum(tarea3)/len(tarea3))
-    diccionario_promedios = {
-        "hw1": promedio1,
-        "hw2": promedio2,
-        "hw3": promedio3
-    }
-    return diccionario_promedios
+    if not students:
+        return {}
+    else:
+        for valor in students.values():
+            tarea1.append(valor["hw1"])
+            tarea2.append(valor["hw2"])
+            tarea3.append(valor["hw3"])
+        promedio1 = round(sum(tarea1)/len(tarea1))
+        promedio2 = round(sum(tarea2)/len(tarea2))
+        promedio3 = round(sum(tarea3)/len(tarea3))
+        diccionario_promedios = {
+            "hw1": promedio1,
+            "hw2": promedio2,
+            "hw3": promedio3
+        }
+        return diccionario_promedios
